f8687ebbd086ec0f2c10f8ed02e1c59823d8fc7c
ritik1jain <ritikjain0706@gmail.com>
2020-10-02 10:55:05 +0530

    final setup

--- src/App.js
+++ src/App.js
@@ -1,78 +1,130 @@
-import React, { Component} from 'react';
-import {Label,Input,Button, Form, FormGroup, Col} from 'reactstrap';
+import React, { Component } from 'react';
+import {
+  Label,
+  Input,
+  Button,
+  Form,
+  FormGroup,
+  Col,
+  Row,
+  FormFeedback,
+} from 'reactstrap';
 import Result from './Result';
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			origin:'',
-			dest:'',
-			showResult: false
-		}
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.toggleResult = this.toggleResult.bind(this);
-	}
+  constructor() {
+    super();
+    this.state = {
+      origin: '',
+      dest: '',
+      showResult: false,
+      touched: {
+        origin: false,
+        dest: false
+      }
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleResult = this.toggleResult.bind(this);
+    this.handleBlurr = this.handleBlurr.bind(this);
+  }
 
-	handleChange(e) {
-		this.setState({
-			[e.target.name]: e.target.value
-		});
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
 
-	}
+  handleSubmit(e) {
+    e.preventDefault();
+    this.toggleResult();
+  }
 
-	handleSubmit(e) {
-		e.preventDefault();
-		this.toggleResult();
-	}
+  toggleResult() {
+    this.setState({ showResult: true });
+  }
 
-	toggleResult() {
-		this.setState({showResult: !this.state.showResult});
-	}
+  handleBlurr = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  }
 
-	render() {
-		let showResult = this.state.showResult;
+  validate(origin, dest) {
+    const errors = {
+      origin: '',
+      dest: '',
+    };
+    if (this.state.touched.origin && origin.length !== 6)
+      errors.origin = 'Provide valid 6 digit pincode';
+    if (this.state.touched.dest && dest.length !== 6)
+      errors.dest = 'Provide valid 6 digit pincode';
 
-		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-12">
-						<h3>Distance between zipcodes</h3>
-					</div>
-					<div className="col-12 col-md-9">
-						<Form onSubmit={this.handleSubmit}>
-							<FormGroup row>
-								<Label htmlFor="origin" md={2}>Origin</Label>
-								<Col md={10}>
-									<Input type="text" id="origin" name="origin" value={this.state.origin}
-									onChange={this.handleChange} />
-								</Col>
-							</FormGroup>
-							<FormGroup row>
-								<Label htmlFor="dest" md={2}>Destination</Label>
-								<Col md={10}>
-									<Input type="text" id="dest" name="dest" value={this.state.dest}
-									onChange={this.handleChange} />
-								</Col>
-							</FormGroup>
-							<FormGroup row>
-								<Col md={{size: 10, offset: 2}}>
-									<Button type="submit" color="primary" >Calculate</Button>
-								</Col>
-							</FormGroup>
-						</Form>
-					</div>
-					{showResult &&
-					<Result state={{...this.state}} ></Result>
-					}
-				</div>
-			</div>
-
-		);
-	}
+    return errors;
+  }
+  render() {
+    let showResult = this.state.showResult;
+    const errors = this.validate(this.state.origin, this.state.dest);
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <h3>Distance between zipcodes</h3>
+          </div>
+          <div className='col-12 col-md-9'>
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup row>
+                <Label htmlFor='origin' md={2}>
+                  Origin
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type='text'
+                    id='origin'
+                    name='origin'
+                    value={this.state.origin}
+                    valid={errors.origin === ''}
+					invalid={errors.dest !== ''}
+					onBlur={this.handleBlurr('origin')}
+					onChange={this.handleChange}
+					required={true}
+                  />
+				  <FormFeedback>{errors.origin}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor='dest' md={2}>
+                  Destination
+                </Label>
+                <Col md={10}>
+				<Input
+                    type='text'
+                    id='dest'
+                    name='dest'
+                    value={this.state.dest}
+                    valid={errors.dest === ''}
+					invalid={errors.dest !==''}
+					onBlur={this.handleBlurr('dest')}
+					onChange={this.handleChange}
+					required={true}
+                  />
+				  <FormFeedback>{errors.dest}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col md={{ size: 10, offset: 2 }}>
+                  <Button type='submit' color='primary'>
+                    Calculate
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </div>
+          {showResult && <Result state={{ ...this.state }}></Result>}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
-
-
--- src/Result.js
+++ src/Result.js
@@ -1,53 +1,102 @@
-import React, {Component} from 'react';
- 
+import React, { Component } from 'react';
+import { Loading } from './Loading';
 import axios from 'axios';
 import GOOGLE_MAPS_API_KEY from './secrets';
 
 class Result extends Component {
-    constructor(props) {
-        super(props);
-        
-    }
-    render() {
-        let distance;
-        let time;
-        let latorigin;
-        let lngorigin;
-        let latdest;
-        let lngdest;
-        const {origin, dest} = this.props.state;
-        axios.get(`https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=${GOOGLE_MAPS_API_KEY}&searchtext=${origin}`)
-        .then(function(res){
+  constructor(props) {
+    super(props);
+    this.state = {
+      distance: null,
+      time: null,
+      loading: true,
+    };
+  }
+  
+  componentWillMount() {
+    let distance;
+    let time;
+    let latorigin;
+    let lngorigin;
+    let latdest;
+    let lngdest;
+    const { origin, dest } = this.props.state;
+    const self = this;
+    axios
+      .get(
+        `https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=${GOOGLE_MAPS_API_KEY}&searchtext=${origin}`
+      )
+      .then(function (res) {
+        console.log(res);
+        latorigin =
+          res.data.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+        lngorigin =
+          res.data.Response.View[0].Result[0].Location.DisplayPosition
+            .Longitude;
+
+        axios
+          .get(
+            `https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=${GOOGLE_MAPS_API_KEY}&searchtext=${dest}`
+          )
+          .then(function (res) {
             console.log(res);
-           latorigin = res.data.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
-             lngorigin = res.data.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
-             
-        axios.get(`https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=${GOOGLE_MAPS_API_KEY}&searchtext=${dest}`)
-        .then(function(res){
-            console.log(res);
-            latdest = res.data.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
-            lngdest = res.data.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
-            
-        axios.get(`https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey=${GOOGLE_MAPS_API_KEY}&waypoint0=geo!${latorigin},${lngorigin}&waypoint1=geo!${latdest},${lngdest}&mode=fastest;car;traffic:disabled`)
-        .then(function(response) {
-                time = response.data.response.route[0].summary.baseTime
-                distance= response.data.response.route[0].summary.distance
-        
-            console.log(time, distance);
-        })})})
-        .catch(function(error) {
-            console.log(error);
-        })
-        return(
-            <div className="col-12 col-md-9">
-                <h4> Distance:</h4>
-                <p>{distance}</p>
-                <h4>Travel duration:</h4>
-                <p>{time}</p>
-            </div>
-        );
+            latdest =
+              res.data.Response.View[0].Result[0].Location.DisplayPosition
+                .Latitude;
+            lngdest =
+              res.data.Response.View[0].Result[0].Location.DisplayPosition
+                .Longitude;
+
+            axios
+              .get(
+                `https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey=${GOOGLE_MAPS_API_KEY}&waypoint0=geo!${latorigin},${lngorigin}&waypoint1=geo!${latdest},${lngdest}&mode=fastest;car;traffic:disabled`
+              )
+              .then(function (response) {
+                time = response.data.response.route[0].summary.baseTime;
+                distance = response.data.response.route[0].summary.distance;
+                let hr,min,km,m;
+                if(distance>1000){
+                  km = Math.floor(distance/1000);
+                  m = distance%1000;
+                  distance = km + ' Km ' + m + ' m';
+                }
+                if(time>3600)
+                {
+                  hr = Math.floor(time/3600);
+                  min= Math.floor((time-hr*3600)/60)
+                  time= hr + ' Hr ' + min + ' min';
+                } else if (time>60){
+                  min = Math.floor(time/60);
+                  time= min + ' mins';
+                }
+
+                console.log(response);
+                self.setState({
+                    distance: distance,
+                    time: time,
+                    loading: false,
+                  });
+                
+              });
+          });
+          
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  render() {
+    
+      return (
+        <div className='col-12 col-md-9'>
+          <h4> Distance:</h4>
+          {this.state.loading ? <Loading /> : this.state.distance}
+          <h4>Travel duration:</h4>
+          {this.state.loading ? <Loading /> : this.state.time}
+        </div>
+      );
     }
-}
+  }
 
 
 export default Result;
